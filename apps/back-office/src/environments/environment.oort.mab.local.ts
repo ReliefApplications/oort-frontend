32d1e854f41f13408d04a080e889637c5b0cd274
Antoine Hurard <antoine@reliefapplications.org>
2025-08-05 14:54:46 +0200

    add mab local configurations

---
+++ apps/back-office/src/environments/environment.oort.mab.local.ts
@@ -0,0 +1,46 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+import { theme } from '../themes/oort/oort.prod';
+import { sharedEnvironment } from './environment.shared';
+import { Environment } from './environment.type';
+
+/**
+ * Authentication configuration
+ */
+const authConfig: AuthConfig = {
+  issuer: 'https://id-mab.unesco.oortcloud.tech/realms/oort',
+  redirectUri: 'http://localhost:4200/',
+  postLogoutRedirectUri: 'http://localhost:4200/auth/',
+  clientId: 'mab-client',
+  scope: 'openid profile email offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+};
+
+/**
+ * Environment file for local development.
+ */
+export const environment: Environment = {
+  ...sharedEnvironment,
+  production: true,
+  apiUrl: 'https://mab.unesco.oortcloud.tech/api',
+  subscriptionApiUrl: 'wss://mab.unesco.oortcloud.tech/api',
+  frontOfficeUri: 'https://mab.unesco.oortcloud.tech',
+  backOfficeUri: 'https://mab.unesco.oortcloud.tech/admin/',
+  availableLanguages: ['en', 'fr'],
+  authConfig,
+  theme,
+  user: { attributes: ['unescoSector'] },
+  availableWidgets: [
+    'form',
+    'donut-chart',
+    'line-chart',
+    'bar-chart',
+    'column-chart',
+    'pie-chart',
+    'grid',
+    'text',
+    'map',
+    'summaryCard',
+    'tabs',
+  ],
+};
