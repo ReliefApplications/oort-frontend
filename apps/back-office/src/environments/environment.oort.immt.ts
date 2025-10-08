a2fef28244cb6e8753e67f669af4dc0300ffc757
Antoine Hurard <50317271+AntoineRelief@users.noreply.github.com>
2025-10-08 12:21:19 +0200

    refactor: use french format by default for dates with kendo intl (#200)
    MFLP-38 
    
    Co-authored-by: ioan-oort <ioan@reliefapplications.org>

--- apps/back-office/src/environments/environment.oort.immt.ts
+++ apps/back-office/src/environments/environment.oort.immt.ts
@@ -29,6 +29,7 @@ export const environment: Environment = {
   frontOfficeUri: 'https://pubplanner.unesco.org',
   backOfficeUri: 'https://immt.unesco.oortcloud.tech/admin/',
   availableLanguages: ['en', 'fr'],
+  defaultIntlLocale: 'fr',
   authConfig,
   theme,
   user: { attributes: ['unescoSector'] },
