ac5f19fa3052b03b900597b7e5963791ab56410c
Antoine Hurard <antoine@reliefapplications.org>
2026-01-07 17:17:18 +0100

    Improve performance of the grid

---
+++ libs/shared/src/lib/components/ui/core-grid/pipes/inline-actions/inline-actions.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { InlineActionsPipe } from './inline-actions.pipe';
+
+describe('InlineActionsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new InlineActionsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
