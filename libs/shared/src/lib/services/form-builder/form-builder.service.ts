40864ab915c7290ec190ca2333868d160300c812
Ronny <ronnycapriles47@gmail.com>
2026-01-13 10:42:15 -0400

    fix: Increase snackbar duration

--- libs/shared/src/lib/services/form-builder/form-builder.service.ts
+++ libs/shared/src/lib/services/form-builder/form-builder.service.ts
@@ -712,7 +712,7 @@ export class FormBuilderService {
           error: (error: HttpErrorResponse) => {
             this.snackBar.openSnackBar(error.message, {
               error: true,
-              duration: 15000,
+              duration: 25000,
             });
             options.callback(null, error);
           },
